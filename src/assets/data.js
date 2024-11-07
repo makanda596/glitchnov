913eca076a38e677e373f3fd45e0a507b2c67b15
makanda596 <149590359+makanda596@users.noreply.github.com>
2024-11-07 12:40:40 +0300

    added assets folder

---
+++ src/assets/data.js
@@ -0,0 +1,841 @@
+
+import image1 from '../assets/images/image1.jpg';
+import image2 from '../assets/images/image2.jpg';
+import image3 from '../assets/images/image3.jpg';
+import image4 from '../assets/images/image4.jpg';
+import image5 from '../assets/images/image5.jpg';
+import image6 from '../assets/images/image6.jpg'
+import model1 from '../assets/models shoot/model1.jpg'
+import model9 from '../assets/models shoot/model9.jpg'
+import model7 from '../assets/models shoot/model7.jpg'
+import model8 from '../assets/models shoot/model8.jpg'
+import wed9 from '../assets/Events/weddings & Ruracios/wed9.jpg'
+import event2 from '../assets/Events/cooperate events/event2.jpg'
+import event8 from '../assets/Events/cooperate events/event 8.jpg'
+import event1 from '../assets/Events/cooperate events/event1.jpg'
+import event9 from '../assets/Events/cooperate events/event9.jpg'
+import event4 from '../assets/Events/cooperate events/event4.jpg'
+import event3 from '../assets/Events/cooperate events/event3.jpg'
+import event7 from '../assets/Events/cooperate events/event7.jpg'
+import wed7 from '../assets/Events/weddings & Ruracios/wed7.jpg'
+import wed17 from '../assets/Events/weddings & Ruracios/wed17.jpg'
+import birthday2 from '../assets/birthday shoot-20241015T110124Z-001/birthday shoot/birthday2.jpg'
+import out3 from '../assets/birthday shoot-20241015T110124Z-001/birthday shoot/out3.JPG'
+import outdoor from '../assets/birthday shoot-20241015T110124Z-001/birthday shoot/outdoor.jpg'
+import outdoor1 from '../assets/outdoor shoot/outdoor1.jpg'
+import outdoor11 from '../assets/outdoor shoot/outdoor11.jpg'
+import outdoor6 from '../assets/outdoor shoot/outdoor6.jpg'
+import outdoor7 from '../assets/outdoor shoot/outdoor7.jpg'
+import outdoor5 from '../assets/outdoor shoot/outdoor5.jpg'
+import outdoor3 from '../assets/outdoor shoot/outdoor3.jpg'
+import outdoor4 from '../assets/outdoor shoot/outdoor4.jpg'
+import outdoor14 from '../assets/outdoor shoot/outdoor14.jpg'
+import outdoor17 from '../assets/outdoor shoot/outdoor17.jpg'
+import outdoor18 from '../assets/outdoor shoot/outdoor18.jpg'
+import outdoor9 from '../assets/outdoor shoot/outdoor9.jpg'
+import outdoor8 from '../assets/outdoor shoot/outdoor8.jpg'
+import outdoor16 from '../assets/outdoor shoot/outdoor16.jpg'
+import outdoor13 from '../assets/outdoor shoot/outdoor13.jpg'
+import outdoor10 from '../assets/outdoor shoot/outdoor10.jpg'
+import outdoor12 from '../assets/outdoor shoot/outdoor12.JPG'
+import outdoor15 from '../assets/outdoor shoot/outdoor15.jpg'
+import indoor9 from '../assets/indoor shoot/indoor9.jpg'
+import indoor4 from '../assets/indoor shoot/indoor4.jpg'
+import indoor6 from '../assets/indoor shoot/indoor6.jpg'
+import indoor5 from '../assets/indoor shoot/indoor5.jpg'
+import indoor7 from '../assets/indoor shoot/indoor7.jpg'
+import indoor11 from '../assets/indoor shoot/indoor11.jpg'
+import indoor8 from '../assets/indoor shoot/indoor8.jpg'
+import indoor10 from '../assets/indoor shoot/indoor10.jpg'
+import indoor12 from '../assets/indoor shoot/indoor12.jpg'
+import indoor13 from '../assets/indoor shoot/indoor13.jpg'
+import couples5 from '../assets/couples shoot/couples5.jpg'
+import product1 from '../assets/product shoot/product1.jpg'
+import poster4 from '../assets/posters/poster4.jpg'
+import creative4 from '../assets/concept & creativity/creative4.jpg'
+import creative13 from '../assets/concept & creativity/creative13.jpg'
+import creative10 from '../assets/concept & creativity/creative10.jpg'
+import creative5 from '../assets/concept & creativity/creative5.jpg'
+import creative6 from '../assets/concept & creativity/creative6.jpg'
+import creative1 from '../assets/concept & creativity/creative1.jpg'
+import wed19 from '../assets/Events/weddings & Ruracios/wed19.jpg'
+import wed18 from '../assets/Events/weddings & Ruracios/wed18.jpg'
+import wed8 from '../assets/Events/weddings & Ruracios/wed8.jpg'
+import wed10 from '../assets/Events/weddings & Ruracios/wed10.jpg'
+import pabiro from '../assets/Events/cooperate events/pabiro-2.mp4'
+import horticulture from '../assets/Events/cooperate events/horticulture.mp4'
+import videowed1 from '../assets/Events/weddings & Ruracios/videowed1.mp4'
+import videowed2 from '../assets/Events/weddings & Ruracios/videowed2.mp4'
+import birthday1 from '../assets/birthday shoot-20241015T110124Z-001/birthday shoot/birthday1.jpg'
+import indoor2 from '../assets/birthday shoot-20241015T110124Z-001/birthday shoot/indo2.jpg'
+import out from '../assets/birthday shoot-20241015T110124Z-001/birthday shoot/out.jpg'
+import couples7 from '../assets/couples shoot/couples7.jpg'
+import model6 from '../assets/models shoot/model6.jpg'
+import out1 from '../assets/birthday shoot-20241015T110124Z-001/birthday shoot/out1.jpg'
+import poster1 from '../assets/posters/poster1.jpg'
+import poster2 from '../assets/posters/poster2.jpg'
+import poster3 from '../assets/posters/poster3.jpg'
+import poster5 from '../assets/posters/poster5.jpg'
+import poster6 from '../assets/posters/poster6.jpg'
+import PPT from '../assets/posters/PPT.jpg'
+
+export const poster = [
+    {
+        id: 1,
+        image: PPT
+    },
+    {
+        id: 1,
+        image: poster1
+    },
+    {
+        id: 1,
+        image: poster2
+    },
+    {
+        id: 1,
+        image: poster3
+    },
+    {
+        id: 1,
+        image: poster4
+    },
+    {
+        id: 1,
+        image: poster5
+    },
+    {
+        id: 1,
+        image: poster6
+    },
+
+
+]
+export const sliding = [
+    {
+        id: 1,
+        image: indoor2,
+        name: "indoor "
+    },
+    {
+        id: 2,
+        image: out3,
+        name: "outdoor"
+    },
+    {
+        id: 3,
+        image: event7,
+        name: "event "
+    },
+    {
+        id: 1,
+        image: creative13,
+        name: "creativity"
+    },
+    {
+        id: 4,
+        image: creative1,
+        name: "potraite "
+    },
+    {
+        id: 5,
+        image: poster2,
+        name: "poster making"
+    },
+    {
+        id: 6,
+        image: outdoor12,
+        name: "poster making"
+    }
+]
+export const outdoors_image = [
+    {
+        id: 1,
+        image: out
+    },
+    {
+        id: 2,
+        image: out1
+    },
+    {
+        id: 2,
+        image: outdoor1
+    },
+    {
+        id: 2,
+        image: model6
+    },
+    {
+        id: 2,
+        image: out3
+    },
+    {
+        id: 19,
+        image: creative13,
+        name: "indoor"
+    },
+    {
+        id: 19,
+        image: creative4,
+        name: "indoor"
+    },
+    {
+        id: 9,
+        image: outdoor
+    },
+    {
+        id: 2,
+        image: outdoor13
+    },
+    {
+        id: 2,
+        image: couples7
+    },
+    {
+        id: 2,
+        image: outdoor7
+    },
+    {
+        id: 2,
+        image: outdoor6
+    },
+    {
+        id: 2,
+        image: outdoor3
+    },
+    {
+        id: 8,
+        image: outdoor12,
+        name: "outdoor"
+    },
+    {
+        id: 7,
+        image: outdoor10,
+        name: "event"
+    },
+    {
+        id: 12,
+        image: indoor9,
+        name: "indoor"
+    },
+
+    {
+        id: 13,
+        image: outdoor15,
+        name: "outdoor"
+    },
+    {
+        id: 14,
+        image: outdoor11,
+        name: "outdoor"
+    },
+    {
+        id: 14,
+        image: outdoor16,
+        name: "outdoor"
+    },
+    {
+        id: 14,
+        image: outdoor17,
+        name: "outdoor"
+    },
+    {
+        id: 19,
+        image: creative10,
+        name: "indoor"
+    },
+    {
+        id: 14,
+        image: outdoor18,
+        name: "outdoor"
+    },
+    {
+        id: 15,
+        image: indoor4,
+        name: "indoor"
+    },
+
+    {
+        id: 16,
+        image: indoor12,
+        name: "indoor"
+    },
+    {
+        id: 16,
+        image: model9,
+        name: "indoor"
+    },
+    {
+        id: 17,
+        image: product1,
+        name: "services"
+    },
+    {
+        id: 18,
+        image: indoor5,
+        name: "indoor"
+    },
+    {
+        id: 18,
+        image: indoor7,
+        name: "indoor"
+    },
+    {
+        id: 19,
+        image: indoor6,
+        name: "indoor"
+    },
+    {
+        id: 19,
+        image: outdoor5,
+        name: "indoor"
+    },
+    {
+        id: 19,
+        image: outdoor8,
+        name: "indoor"
+    },
+    {
+        id: 19,
+        image: outdoor9,
+        name: "indoor"
+    },
+
+    {
+        id: 19,
+        image: outdoor4,
+        name: "indoor"
+    },
+]
+export const events_image = [
+    {
+        id: 1,
+        image: event1
+    },
+    {
+        id: 2,
+        image: birthday1
+    },
+    {
+        id: 3,
+        image: out3
+    },
+    {
+        id: 4,
+        image: wed9
+    },
+    {
+        id: 5,
+        image: event7
+    },
+    {
+        id: 1,
+        image: event9
+    },
+    {
+        id: 1,
+        image: wed17
+    },
+    {
+        id: 1,
+        image: event4
+    },
+    {
+        id: 1,
+        image: event3
+    },
+    {
+        id: 1,
+        image: wed7
+    },
+    {
+        id: 12,
+        image: wed19,
+        name: "wedding"
+    },
+    {
+        id: 3,
+        image: couples5,
+        name: "wedding"
+    },
+    {
+        id: 6,
+        image: event2,
+        name: "event"
+    },
+    {
+        id: 9,
+        image: birthday1,
+        name: "birthday"
+    },
+    {
+        id: 10,
+        image: event8
+    },
+    {
+        id: 9,
+        image: birthday2,
+        name: "birthday"
+    },
+    {
+        id: 10,
+        image: wed10
+    },
+    {
+        id: 10,
+        image: wed8
+    },
+    {
+        id: 10,
+        image: wed18
+    },
+]
+export const loading_more = [
+
+    {
+        id: 11,
+        image: wed19,
+        name: "wedding"
+    },
+    {
+        id: 12,
+        image: indoor9,
+        name: "indoor"
+    },
+
+    {
+        id: 13,
+        image: outdoor15,
+        name: "outdoor"
+    },
+    {
+        id: 14,
+        image: outdoor11,
+        name: "outdoor"
+    },
+    {
+        id: 31,
+        image: poster2,
+        name: "product"
+    },
+    {
+        id: 32,
+        image: poster3,
+        name: "product"
+    },
+    {
+        id: 33,
+        image: poster4,
+        name: "product"
+    },
+    {
+        id: 15,
+        image: indoor4,
+        name: "indoor"
+    },
+    {
+        id: 16,
+        image: indoor12,
+        name: "indoor"
+    },
+    {
+        id: 16,
+        image: model9,
+        name: "indoor"
+    },
+    {
+        id: 17,
+        image: product1,
+        name: "services"
+    },
+    {
+        id: 18,
+        image: indoor5,
+        name: "indoor"
+    },
+    {
+        id: 19,
+        image: indoor6,
+        name: "indoor"
+    },
+    {
+        id: 20,
+        image: indoor8,
+        name: "indoor"
+    },
+    {
+        id: 21,
+        image: indoor10,
+        name: "indoor"
+    },
+    {
+        id: 22,
+        image: indoor11,
+        name: "indoor"
+    },
+    {
+        id: 23,
+        image: outdoor5,
+        name: "outdoor"
+    },
+    {
+        id: 24,
+        image: outdoor6,
+        name: "outdoor"
+    },
+    {
+        id: 25,
+        image: outdoor14,
+        name: "outdoor"
+    },
+    {
+        id: 26,
+        image: outdoor13,
+        name: "outdoor"
+    },
+    {
+        id: 27,
+        image: outdoor16,
+        name: "outdoor"
+    },
+    {
+        id: 28,
+        image: outdoor4,
+        name: "outdoor"
+    },
+    {
+        id: 29,
+        image: product1,
+        name: "product"
+    },
+    {
+        id: 30,
+        image: poster1,
+        name: "product"
+    },
+
+    {
+        id: 34,
+        image: poster5,
+        name: "product"
+    },
+    {
+        id: 29,
+        image: PPT,
+        name: "product"
+    }
+]
+export const outdoor_image = [
+    {
+        id: 1,
+        image: out
+    },
+    {
+        id: 2,
+        image: out1
+    },
+    {
+        id: 2,
+        image: model6
+    },
+    {
+        id: 2,
+        image: out3
+    },
+
+
+    {
+        id: 9,
+        image: outdoor
+    },
+    {
+        id: 2,
+        image: outdoor13
+    },
+    {
+        id: 2,
+        image: couples7
+    },
+    {
+        id: 2,
+        image: outdoor7
+    },
+    {
+        id: 2,
+        image: outdoor6
+    },
+    {
+        id: 2,
+        image: outdoor3
+    },
+]
+export const event_image = [
+    {
+        id: 1,
+        image: event1
+    },
+    {
+        id: 2,
+        image: birthday1
+    },
+    {
+        id: 3,
+        image: out3
+    },
+    {
+        id: 4,
+        image: wed9
+    },
+    {
+        id: 5,
+        image: event7
+    },
+    {
+        id: 1,
+        image: event9
+    },
+    {
+        id: 1,
+        image: wed17
+    },
+    {
+        id: 1,
+        image: event4
+    },
+    {
+        id: 1,
+        image: event3
+    },
+    {
+        id: 1,
+        image: wed7
+    },
+
+]
+export const creative_image = [
+    {
+        id: 1,
+        image: creative1
+    },
+    {
+        id: 2,
+        image: creative4
+    },
+    {
+        id: 3,
+        image: creative10
+    },
+    {
+        id: 4,
+        image: creative13
+    },
+    {
+        id: 5,
+        image: creative6
+    },
+    {
+        id: 6,
+        image: creative5
+    },
+    {
+        id: 7,
+        image: indoor2
+    }
+]
+export const videos = [
+    {
+        id: 1,
+        src: horticulture,
+
+    },
+    {
+        id: 2,
+        src: videowed1,
+    },
+    {
+        id: 1,
+        src: pabiro,
+    },
+    {
+        id: 1,
+        src: videowed2,
+    }
+]
+export const all_image = [
+    {
+        id: 1,
+        image: model1,
+        name: "creativity"
+    },
+
+    {
+        id: 2,
+        image: model7,
+        name: "creativity"
+    },
+    {
+        id: 3,
+        image: couples5,
+        name: "wedding"
+    },
+
+    {
+        id: 5,
+        image: model8,
+        name: "creativity"
+    },
+    {
+        id: 6,
+        image: event2,
+        name: "event"
+    },
+    {
+        id: 8,
+        image: outdoor12,
+        name: "outdoor"
+    },
+    {
+        id: 7,
+        image: outdoor10,
+        name: "event"
+    },
+
+    {
+        id: 9,
+        image: birthday1,
+        name: "birthday"
+    },
+    {
+        id: 12,
+        image: outdoor,
+        name: "outdoor"
+    },
+    {
+        id: 10,
+        image: indoor13,
+        name: "indoor"
+    },
+
+]
+export const load_more = [
+
+    {
+        id: 11,
+        image: wed19,
+        name: "wedding"
+    },
+    {
+        id: 12,
+        image: indoor9,
+        name: "indoor"
+    },
+
+    {
+        id: 13,
+        image: outdoor15,
+        name: "outdoor"
+    },
+    {
+        id: 14,
+        image: outdoor11,
+        name: "outdoor"
+    },
+    {
+        id: 15,
+        image: indoor4,
+        name: "indoor"
+    },
+    {
+        id: 16,
+        image: indoor12,
+        name: "indoor"
+    },
+    {
+        id: 16,
+        image: model9,
+        name: "indoor"
+    },
+    {
+        id: 17,
+        image: product1,
+        name: "services"
+    },
+    {
+        id: 18,
+        image: indoor5,
+        name: "indoor"
+    },
+    {
+        id: 19,
+        image: indoor6,
+        name: "indoor"
+    },
+]
+export const images = [
+    require('./outdoor1.jpg'),
+    require('./wed3.jpg'),
+    require('./cooperate1.jpg'),
+    require('./wed1.jpg'),
+    require('./creative1.jpg'),
+];
+
+export const clientImages = [
+    {
+        id: 1,
+        image: image1,
+        name: "dfdsddfdf"
+    },
+    {
+        id: 2,
+        image: image2,
+        name: "dfdf"
+    },
+    {
+        id: 3,
+        image: image3,
+        name: "dfdf"
+    },
+    {
+        id: 4,
+        image: image4,
+        name: "dmnfnrfn"
+    },
+    {
+        id: 5,
+        image: image5,
+        name: "nbdkdj"
+    },
+    {
+        id: 6,
+        image: image6,
+    },
+    {
+        id: 11,
+        image: image1,
+        name: "dfdsddfdf"
+    },
+    {
+        id: 12,
+        image: image2,
+        name: "dfdf"
+    },
+    {
+        id: 3,
+        image: image3,
+        name: "dfdf"
+    },
+    {
+        id: 14,
+        image: image4,
+        name: "dmnfnrfn"
+    },
+    {
+        id: 15,
+        image: image5,
+        name: "nbdkdj"
+    },
+    {
+        id: 16,
+        image: image6,
+    },
+];
