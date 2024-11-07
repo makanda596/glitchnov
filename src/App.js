4adc3c0ae3b039fa31149a0990c547c67f465107
makanda596 <149590359+makanda596@users.noreply.github.com>
2024-11-07 14:18:13 +0300

    changed the app.js and .css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home'
+import Contact from './Pages/Contact/Contact'
+import Creative from './Pages/Creative/Creative'
+import Events from './Pages/Events/Events'
+import Gallery from './Pages/Gallery/Gallery'
+import Outdoor from './Pages/Outdoor/Outdoor'
+import Services from './Pages/Services/Services'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/creative" element={<Creative />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/outdoor" element={<Outdoor />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
     </div>
   );
 }
